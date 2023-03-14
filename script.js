87c08b31be12a2ba972522b8fb53a640669a6072
Jeremy Woods <jeremywoods124@gmail.com>
2023-03-14 15:29:53 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,61 @@
+const menu = document.querySelector('#mobile-menu');
+const menuLinks = document.querySelector('.navbar__menu');
+const navLogo = document.querySelector('#navbar__logo');
+
+// display mobile menu
+const mobileMenu = () => {
+    menu.classList.toggle('is-active');
+    menuLinks.classList.toggle('active');
+}
+
+menu.addEventListener('click', mobileMenu);
+
+const highlightMenu = () => {
+    const elem = document.querySelector('.highlight');
+    const homeMenu = document.querySelector('#home-page');
+    const aboutMenu = document.querySelector('#about-page');
+    const servicesMenu = document.querySelector('#tools-page');
+    const projectsMenu = document.querySelector('#projects-page');
+    let scrollPos = window.scrollY;
+    console.log(scrollPos);
+  
+    // adds 'highlight' class to my menu items
+    if (window.innerWidth > 960 && scrollPos < 400) {
+      homeMenu.classList.add('highlight');
+      aboutMenu.classList.remove('highlight');
+      return;
+    } else if (window.innerWidth > 960 && scrollPos < 800) {
+      aboutMenu.classList.add('highlight');
+      homeMenu.classList.remove('highlight');
+      servicesMenu.classList.remove('highlight');
+      return;
+    } else if (window.innerWidth > 960 && scrollPos < 1400) {
+      servicesMenu.classList.add('highlight');
+      aboutMenu.classList.remove('highlight');
+      projectsMenu.classList.remove('highlight');
+      return;
+    } else if (window.innerWidth > 960 && scrollPos < 2600) {
+        projectsMenu.classList.add('highlight');
+        servicesMenu.classList.remove('highlight');
+        return;
+      }
+  
+    if ((elem && window.innerWIdth < 960 && scrollPos < 600) || elem) {
+      elem.classList.remove('highlight');
+    }
+  };
+  
+  window.addEventListener('scroll', highlightMenu);
+  window.addEventListener('click', highlightMenu);
+
+  // close mobile menu when clicking on a menu item
+  const hideMobileMenu = () => {
+    const menuBars = document.querySelector('.is-active');
+    if (window.innerWidth <= 768 && menuBars) {
+        menu.classList.toggle('is-active');
+        menuLinks.classList.remove('active');
+    }
+  }
+
+  menuLinks.addEventListener('click', hideMobileMenu);
+  navLogo.addEventListener('click', hideMobileMenu);
